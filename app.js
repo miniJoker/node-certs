efd7c78c1bd33ff5f0bfaab1db1e945c50c4b32f
Andrey <lenkovez.as@gmail.com>
2021-03-04 12:10:09 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require("fs");
 
 const hostname = '127.0.0.1';
-const port = 8080;
+const port = process.env.PORT || 3000;
 let data = JSON.parse(fs.readFileSync('./data.json', 'utf-8'));
 let user = undefined;
 
